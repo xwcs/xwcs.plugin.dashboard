8b8125118fb89624e4b72be4ee65a637bbb55d9a
PavolPala <palap@ipoint.sk>
2016-02-10 14:19:35 +0100

    Devel in progress workspace and layouts finished.

--- Dashboard.cs
+++ Dashboard.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using xwcs.core.plgs;
 using xwcs.core.evt;
+using xwcs.core.ui.controls;
 
 namespace xwcs.plugin.dashboard
 {
@@ -17,28 +18,28 @@ namespace xwcs.plugin.dashboard
             Info.addAbility(pluginAbility.PLGABLT_usercontrol);
             Info.addControl(new xwcs.core.controls.VisualControlInfo("Dashboard Panel", typeof(DashboardControl)));
             Info.addControl(new xwcs.core.controls.VisualControlInfo("OutputControl", typeof(supp.OutputControl)));
-        }
-
-        
-
+            Info.addControl(new xwcs.core.controls.VisualControlInfo("TestControl", typeof(supp.TestControl)));
+        }      
        
 
         public override void afterInit()
         {
-            //EventProxy.addEventHandler(DashboardEventType.UserManagementToolBarEvent, HandlePlugin1ToolBarEvent);
-
-
-            MenuAddRequest[] menuadd = new MenuAddRequest[2];
+            MenuAddRequest[] menuadd = new MenuAddRequest[3];
 
             xwcs.core.controls.VisualControlInfo vic = Info.getVisualControlInfoByGuid(Guid.Parse(DashboardControl.GUID));
-            XwBarButtonItem barButtonItem1 = new XwBarButtonItem(vic.Name);
+            XwBarButtonItem barButtonItem1 = new XwBarButtonItem(vic.Name, 1);
             barButtonItem1.ItemClick += buttonClick1;
             menuadd[0] = new MenuAddRequest { destination = MenuDestination.MENU_file_open, content = barButtonItem1 };
 
             vic = Info.getVisualControlInfoByGuid(Guid.Parse(supp.OutputControl.GUID));
-            XwBarButtonItem barButtonItem2 = new XwBarButtonItem(vic.Name);
+            XwBarButtonItem barButtonItem2 = new XwBarButtonItem(vic.Name, 2);
             barButtonItem2.ItemClick += buttonClick2;
             menuadd[1] = new MenuAddRequest { destination = MenuDestination.MENU_ViewOtherWindows, content = barButtonItem2 };
+
+            vic = Info.getVisualControlInfoByGuid(Guid.Parse(supp.TestControl.GUID));
+            XwBarButtonItem barButtonItem3 = new XwBarButtonItem(vic.Name, 3);
+            barButtonItem3.ItemClick += buttonClick3;
+            menuadd[2] = new MenuAddRequest { destination = MenuDestination.MENU_ViewOtherWindows, content = barButtonItem3 };
 
             EventProxy.fireEvent(new AddToolBarRequestEvent(this, new AddToolBarRequest(menuadd)));
         }
@@ -46,25 +47,24 @@ namespace xwcs.plugin.dashboard
 
         private void buttonClick1(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
-            //EventProxy.fireEvent(new DashboardToolBarEvent(this, Guid.Parse(DashboardControl.GUID)));
-
             OpenPanelRequest panelRequest = new OpenPanelRequest { Vci = Info.getVisualControlInfoByGuid(Guid.Parse(DashboardControl.GUID)) };
             OpenPanelRequestEvent openPanelEvent = new OpenPanelRequestEvent(this, panelRequest);
-
             EventProxy.fireEvent(openPanelEvent);
-
         }
 
         private void buttonClick2(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
-            //EventProxy.fireEvent(new DashboardToolBarEvent(this, Guid.Parse(DashboardControl.GUID)));
-
             OpenPanelRequest panelRequest = new OpenPanelRequest { Vci = Info.getVisualControlInfoByGuid(Guid.Parse(supp.OutputControl.GUID)) };
             OpenPanelRequestEvent openPanelEvent = new OpenPanelRequestEvent(this, panelRequest);
-
             EventProxy.fireEvent(openPanelEvent);
         }
 
-       
+        private void buttonClick3(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            OpenPanelRequest panelRequest = new OpenPanelRequest { Vci = Info.getVisualControlInfoByGuid(Guid.Parse(supp.TestControl.GUID)) };
+            OpenPanelRequestEvent openPanelEvent = new OpenPanelRequestEvent(this, panelRequest);
+            EventProxy.fireEvent(openPanelEvent);
+        }
+
     }
 }
--- supp/TestControl.cs
+++ supp/TestControl.cs
@@ -8,28 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using xwcs.core.ui.controls;
-using xwcs.core.evt;
 
 namespace xwcs.plugin.dashboard.supp
 {
-    public partial class OutputControl : VisualControl
+    public partial class TestControl : VisualControl
     {
         /* static control description */
-        public const string GUID = "b14cd054-534d-4f6a-9ca8-53b753e5a1de";
+        public const string GUID = "7f17f42a-082d-49a1-9783-0c5fda45b9b6";
         public const string VERSION = "1.0";
         public const xwcs.core.controls.ControlDockStyle DOCK_STYLE = xwcs.core.controls.ControlDockStyle.PLGT_status;
 
-        public OutputControl()
+
+        public TestControl()
         {
             InitializeComponent();
-            SEventProxy.getInstance().addEventHandler(EventType.OutputMessageEvent, HandleOutputMessageEvent);
-        }
-
-        private void HandleOutputMessageEvent(Event e)
-        {
-
-            richTextBox1.Text += ((OutputMessageEvent)e).Message;
-            richTextBox1.Text += "\n";
         }
     }
 }
